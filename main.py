3a753cbd09640b257cb0883060b794480d8fe457
Юлия <kornyakova_11@mail.ru>
2024-08-01 13:59:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+"base structure information"
+"1st program"
+print((9 ** 0.5) * 5)  # / / - целочисленное деление , / - деление
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)  # boolean
+"3rd program"
+print(((1234 % 1000) // 10) + ((5678 % 1000) // 10))  #  -  % - остаточное деление , / / - целочисленное деление
+"4th program"
+print(1342 // 100 == 4213 % 100 or 4213 // 100 == 1342 % 100)  # boolean
